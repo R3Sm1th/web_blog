7d4c1785e1e887a5e1d3b8fe8ff002cb1f6c0672
Russell Hallowes-Smith <russellsmith6@gmail.com>
2024-06-26 20:12:38 +0100

    draft option

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft returns true for a draft" do
+    binding.rb
+    assert BlogPost.new(title: "Draft", body: "Draft body", published_at: nil).draft?
+  end
 end
