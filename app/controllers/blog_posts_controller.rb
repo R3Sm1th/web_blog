1158a42cfbeba2e81c10c67f2db113de098c79cf
Russell Hallowes-Smith <russellsmith6@gmail.com>
2024-06-26 19:54:11 +0100

    scoping

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,9 +2,10 @@ class BlogPostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_blog_post, only: [:show, :edit, :destroy]
   def index
-      @blog_posts = BlogPost.all
+      @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
   end
   def show
+    @blog_post = BlogPost.published.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     root_path
   end
@@ -36,9 +37,11 @@ class BlogPostsController < ApplicationController
   private
 
   def set_blog_post
-    @blog_post = BlogPost.find(params[:id])
+    @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : BlogPost.published.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
   end
   def blog_post_params
-    params.require(:blog_post).permit(:title, :body)
+    params.require(:blog_post).permit(:title, :body, :published_at)
   end
 end
--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,20 @@
 class BlogPost < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+
+  scope :draft, -> { where(published_at: nil, updated_at: :desc) }
+  scope :published, -> { where('published_at <= ?', Time.current) }
+  scope :scheduled, -> { where('published_at > ?', Time.current) }
+
+  scope :sorted, -> { order(published_at: :desc) }
+
+  def draft?
+    published_at.nil?
+  end
+  def published?
+    published_at? && published_at <= Time.current
+  end
+  def scheduled?
+    published_at? && published_at > Time.current
+  end
 end
